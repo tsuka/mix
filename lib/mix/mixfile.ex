d5ebe335ae9e1bd3386e5ecb257ada14dc716731
Anthony Grimes <i@raynes.me>
2012-04-02 03:26:44 -0500

    Mechanisms for loading a project conig definition with defaults.

--- lib/mix/mixfile.ex
+++ lib/mix/mixfile.ex
@@ -7,6 +7,26 @@ defmodule Mix.Mixfile do
   Load a mix project file.
   """
   def load() do
-    Code.require_file("mix")
+    if :filelib.is_file("mix.exs") do
+      Code.require_file("mix")
+    end
+  end
+
+  @doc """
+  Load the project.
+  """
+  def get_project() do
+    case :code.ensure_loaded(Mix.Project) do
+    match: {:module, _}
+      Keyword.merge Mix.Project.project, project_defaults 
+    else:
+      nil
+    end
+  end
+
+  defp project_defaults() do
+    [compile_path: "exbin/",
+     test_paths: ["test/"],
+     source_paths: ["lib/"]]
   end
 end
