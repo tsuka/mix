2e702e426bef3ddf43c2d54bd92685e9ca310c4f
Anthony Grimes <i@raynes.me>
2012-04-03 02:41:41 -0500

    Add a clean task.

---
+++ lib/mix/tasks/clean.ex
@@ -0,0 +1,28 @@
+defmodule Mix.Tasks.Clean do
+  @behavior Mix.Task
+  @shortdoc "Delete compile path and target."
+  @moduledoc """
+  Deletes the files in compile path and target path.
+  This has the effect of wiping all generated artifacts
+  and .beam files.
+  """
+  def run(_) do
+    project = Mix.Mixfile.get_project
+    compile = project[:compile_path]
+    target  = project[:target_path]
+    IO.puts Enum.join(["Deleting files in ", compile, "..."])
+    delete_in_dir compile
+    IO.puts Enum.join(["Deleting files in ", target, "..."])
+    delete_in_dir target
+  end
+
+  defp delete_in_dir(dir) do
+    Enum.each(List.reverse(File.wildcard(File.join([dir, "**"]))), fn(file) ->
+      if :filelib.is_dir(file) do
+        :file.del_dir file
+      else:
+        :file.delete file
+      end
+    end)
+  end
+end
