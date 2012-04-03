7c691d1fd3e781eb61a062965d448a113d17e50c
Anthony Grimes <i@raynes.me>
2012-04-03 15:11:47 -0500

    Add options support. It can at least compile mix now.

--- lib/mix/tasks/compile.ex
+++ lib/mix/tasks/compile.ex
@@ -26,27 +26,31 @@ defmodule Mix.Tasks.Compile do
 
   This will cause foo.ex and bar.ex to be compiled before any
   other files and in the order they appear in the list.
+
+  If you need to pass compilation options, set them in your
+  project under the compile_options key.
   """
   def run(_) do
     project = Mix.Mixfile.get_project
     compile_path = project[:compile_path]
     compile_first = project[:compile_first]
+    options = project[:compile_options]
     if !Enum.empty?(compile_first) do
       IO.puts "Performing initial compilation (compile_first)..."
-      Enum.each(compile_first, compile_file(&1, compile_path))
+      Enum.each(compile_first, compile_file(&1, compile_path, options))
     end
     Enum.each(project[:source_paths], fn(path) ->
       files = File.wildcard(File.join([path, "**/*.ex"]))
       Enum.each(files, fn(file) ->
         if !Enum.find(project[:compile_first], fn(x) -> x == file end) do
-          compile_file(file, compile_path)
+          compile_file(file, compile_path, options)
         end
       end)
     end)
   end
 
-  defp compile_file(file, to) do
+  defp compile_file(file, to, options) do
     IO.puts Enum.join(["Compiling ", file, " to ", to, "..."])
-    Code.compile_file_to_dir(file, to)
+    Code.compile_file_to_dir(file, to, options)
   end
 end
