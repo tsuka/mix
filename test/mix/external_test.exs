4f938ca865fe3e45d8eec0dff3c5a9177511457d
Anthony Grimes <i@raynes.me>
2012-04-05 00:14:51 -0500

    Added another test.

--- test/mix/external_test.exs
+++ test/mix/external_test.exs
@@ -1,7 +1,12 @@
 defmodule ExternalTest do
   use ExUnit.Case
+  import Mix.External
 
   test :take_all do
-    assert_equal "foo\n", Mix.External.take_all(Mix.External.run_cmd("echo", ["foo"]))
+    assert_equal "foo\n", take_all(run_cmd("echo", ["foo"]))
+  end
+
+  test :lists do
+    assert_equal "foo\n", take_all(run_cmd('echo', ['foo']))
   end
 end
