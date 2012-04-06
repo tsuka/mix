e0a0e2c3495fb5121ae24ac11854ada7c67c4a37
Anthony Grimes <i@raynes.me>
2012-04-06 05:07:48 -0500

    Added an 'iex' task to start a REPL with project settings.

---
+++ lib/mix/tasks/iex.ex
@@ -0,0 +1,17 @@
+defmodule Mix.Tasks.Iex do
+  @behavior Mix.Task
+  @shortdoc "Start iex with your project's settings."
+  @moduledoc """
+  Starts an iex repl with your project settings.
+  Your code will be available to iex because mix
+  will add your compile_path to the code path.
+  Will compile first.
+  """
+  def run(_) do
+    project = Mix.Mixfile.get_project
+    Mix.Tasks.run_task "compile"
+    Code.append_path(project[:compile_path])
+    Elixir.IEx.start
+    :timer.sleep(:infinity)
+  end
+end
