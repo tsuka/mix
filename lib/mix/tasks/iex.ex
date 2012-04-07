06bd202f89110cde9aa9a42430f20b62f57b653e
Anthony Grimes <i@raynes.me>
2012-04-07 11:26:23 -0500

    Using erlang_compile_path too.

--- lib/mix/tasks/iex.ex
+++ lib/mix/tasks/iex.ex
@@ -4,13 +4,14 @@ defmodule Mix.Tasks.Iex do
   @moduledoc """
   Starts an iex repl with your project settings.
   Your code will be available to iex because mix
-  will add your compile_path to the code path.
+  will add your `compile_path` and `erlang_compile_path`.
   Will compile first.
   """
   def run(_) do
     project = Mix.Mixfile.get_project
     Mix.Tasks.run_task "compile"
     Code.append_path(project[:compile_path])
+    Code.append_path(project[:erlang_compile_path])
     Elixir.IEx.start
     :timer.sleep(:infinity)
   end
